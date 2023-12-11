7c5e7e956873437c0bdc3b76de6b97f9ca0930e9
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-12-11 09:53:05 +0000

    Construtor e Destrutor de classes

--- 02/aula02.cs
+++ 02/aula02.cs
@@ -1,20 +1,37 @@
 using System;
 
 public class Jogador {
-    public int energia = 100;
-    public bool vivo = true;
+    public int energia;
+    public bool vivo;
+    public string nome;
 
+    public Jogador(string  n) {
+        energia = 100;
+        vivo = true;
+
+        nome = n;
+    }
+
+    ~Jogador(){
+        Console.WriteLine("Jogador {0} foi destruido", nome);
+    }
 }
 
-class Aula02{
+class Aula02 {
     static void Main() {
-        Jogador J1 = new Jogador();
-        Jogador J2 = new Jogador();
-        Jogador J3 = new Jogador();
+
+        Console.Write("Escreva o nome do Jogador 4: ");
+        string nome1 = Console.ReadLine();
+
+        Jogador J1 = new Jogador("Tiago");
+        Jogador J2 = new Jogador("Paulo");
+        Jogador J3 = new Jogador("Lussati");
+        Jogador J4 = new Jogador(nome1);
 
         J1.energia = 40;
-        Console.WriteLine("Energia do jogador 1: {0}", J1.energia);
-        Console.WriteLine("Energia do jogador 2: {0}", J2.energia);
-        Console.WriteLine("Energia do jogador 3: {0}", J3.energia);
+        Console.WriteLine("Nome do jogador 1: {0}", J1.nome);
+        Console.WriteLine("Nome do jogador 2: {0}", J2.nome);
+        Console.WriteLine("Nome do jogador 3: {0}", J3.nome);
+        Console.WriteLine("Nome do jogador 4: {0}", J4.nome);
     }
 }
