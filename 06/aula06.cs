cf81bddc260bd80e3a89395fce426ec74a3dab3a
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-12-26 10:20:50 +0000

    public vs private

--- 06/aula06.cs
+++ 06/aula06.cs
@@ -16,11 +16,32 @@ class Jogador{
     public string getNome() {
         return nome;
     }
+
+    public void setEnergia(int e) {
+        if (e < 0) {
+            if (energia-e < 0) {
+                energia = 0;
+            }
+        } else if (e > 0) {
+            energia += e;
+        }
+
+        if (e > 0) {
+            if (energia + e > 100) {
+                energia = 100;
+            }
+        } else if (e < 0) {
+            energia += e;
+        }
+    }
 }
 
 class Aula06 {
     static void Main() {
         Jogador J1 = new Jogador("Lussati");
+
+        //J1.setEnergia(-30);
+        J1.setEnergia(40000);
 
         Console.WriteLine("Nome   : {0}", J1.getNome());
         Console.WriteLine("Energia: {0}", J1.getEnergia());
