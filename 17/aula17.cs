bf5cc685057257406ccef23111a73f73f57be19a
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2024-01-09 17:25:44 +0000

    interface

--- 17/aula17.cs
+++ 17/aula17.cs
@@ -1,7 +1,41 @@
 using System;
 
+public interface Veiculo {
+    void ligar();
+    void desligar();
+    void info();
+}
+
+public interface Combate {
+    void disparar();
+}
+
+class Carro:Veiculo, Combate {
+
+    public bool ligado;
+    private int municao;
+    public Carro(){
+        this.municao = 100;
+    }
+    public void setMunicao(int qtde){
+        this.municao = qtde;
+    }
+    public void ligar(){
+        this.ligado = true;
+    }
+    public void desligar(){
+        this.ligado = false;
+    }
+    public void info(){
+        
+    }
+    public void disparar(){
+
+    }
+}
+
 class Aula17 {
     static void Main(){
-        
+        Carro carro1 = new Carro();
     }
 }
